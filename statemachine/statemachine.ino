12f2e08a717076d81598fc15e2c6822ea44059ee
tinh8801 <huythangn@gmail.com>
2023-07-02 23:36:10 +0700

    hello

---
+++ statemachine/statemachine.ino
@@ -0,0 +1,53 @@
+#define ledPin 2
+#define RESET 0
+#define CHECKTIME 1
+#define CHECKLEDSTATE 2
+#define LEDON 3
+#define LEDOFF 4
+#define BLINK 500
+long batdau;
+long hientai;
+byte state;
+bool ledState;
+void runningFunc();
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(ledPin,OUTPUT);
+digitalWrite(ledPin,HIGH);
+state=RESET;
+}
+
+void loop() {
+  runningFunc();
+}
+
+void runningFunc(){
+
+  switch (state){
+    case RESET:
+    batdau=millis();
+    if(true) state=CHECKTIME;
+    break;
+    case CHECKTIME:
+    hientai=millis();
+    if((hientai-batdau)>BLINK){
+      state=CHECKLEDSTATE;
+    }
+    break;
+    case CHECKLEDSTATE:
+    ledState=digitalRead(ledPin);
+    if(ledState==HIGH) state=LEDON;
+    else state=LEDOFF;
+    break;
+    case LEDON:
+    digitalWrite(ledPin,LOW);
+    if(true) state=RESET;
+    break;
+    case LEDOFF:
+    digitalWrite(ledPin,HIGH);
+    if(true) state=RESET;
+    break;
+    
+  }
+}
---
+++ statemachine_controham/statemachine_controham.ino
@@ -0,0 +1,40 @@
+#define ledPin 2
+#define BLINK 200
+long batdau;
+long hientai;
+void (*runningFunc)();
+bool ledState;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(ledPin,OUTPUT);
+digitalWrite(ledPin,HIGH);
+runningFunc=&resetFunc;
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+runningFunc();
+}
+void resetFunc(){
+  batdau=millis();
+  if(true) runningFunc=&checktimeFunc;
+}
+void checktimeFunc(){
+  hientai=millis();
+  if((hientai-batdau)>BLINK) runningFunc=&checkledstateFunc;
+}
+void checkledstateFunc(){
+  ledState=digitalRead(ledPin);
+  if(ledState==HIGH) runningFunc=&turnonledFunc;
+  else runningFunc=&turnoffledFunc;
+}
+void turnonledFunc(){
+  digitalWrite(ledPin,LOW);
+  if(true) runningFunc=&resetFunc;
+}
+void turnoffledFunc(){
+  digitalWrite(ledPin,HIGH);
+  if(true) runningFunc=&resetFunc;
+}
