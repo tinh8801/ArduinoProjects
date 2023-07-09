db20cdac68be9894fe892d1a8417ab513fb15391
tinh8801 <huythangn@gmail.com>
2023-07-09 17:46:19 +0700

    .

--- ESP8266_TM1650_NTP_Clock/ESP8266_TM1650_NTP_Clock.ino
+++ ESP8266_TM1650_NTP_Clock/ESP8266_TM1650_NTP_Clock.ino
@@ -7,20 +7,27 @@
 #include <DHT.h>
 #include <WiFiManager.h>
 #include "myweb.h"
-#define DHTPIN 14
+#define DHTPIN 13
 #define DHTTYPE DHT11
-#define DATA 4
-#define CLK 5
+#define DATA 12
+#define CLK 14
 #define NUM_DIGITS 4
 
 DHT dht(DHTPIN,DHTTYPE);
 TM1650 module(DATA,CLK,4,true,4,TM1650_DISPMODE_4x8);   // DIO=8, CLK=9, STB=7
 TM16xxDisplay display(&module, NUM_DIGITS);    // TM16xx object, 8 digits
-int timescroll=500;
 int datescroll=500;
-byte brightness=7;
+int brightness;
 bool isNight;
 bool autobrightness=true;
+int * _temphumid;
+char _temp[6]="";
+char _humid[6]="";
+char _heatindex[6]="";
+bool Mode24h=true;
+char _thoigian[6]="";
+String dow[7]={"SUn","MOn","tUE","WEd","tHU","FrI","SAt"};
+String _dow="";
 
 //const char *ssid     = "DDWRT";
 //const char *password = "12347890";
@@ -28,7 +35,7 @@ bool autobrightness=true;
 ESP8266WebServer server(80);
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP,"asia.pool.ntp.org",7*3600,60000);
-String dt="";
+String _datetime="";
 
 String getDate(){
   String kq="";
@@ -40,35 +47,39 @@ String getDate(){
   return kq=String(currDay)+"-"+String(currMonth)+"-"+String(currYear);  
 }
 
-String getTime(){
-String kq="";
-String phut="";
-  kq=String(timeClient.getHours());
-  phut=String(timeClient.getMinutes());
-  if(phut.length()<2){
-    return kq+"h0"+phut;
-  }else{
-    return kq+"h"+phut;  
-  }   
+int * getTime(bool f=true){//true=24h false=12h
+  static int kq[3];
+    kq[0]=timeClient.getSeconds();
+    kq[1]=timeClient.getMinutes();
+    if(f){
+    kq[2]=timeClient.getHours();
+    }else{
+      if(timeClient.getHours()>12){
+        kq[2]=timeClient.getHours()-12;
+      }else{
+        kq[2]=timeClient.getHours();
+      }
+    }
+    return kq;
 }
 
-int getTemp(){
+int * getTempHumid(){
+  static int kq[3];
   float t=dht.readTemperature();
-  if(isnan(t)){
-    return 99;
+  float h=dht.readHumidity();
+  if(isnan(t) || isnan(h)){
+    kq[0]=99;
+    kq[1]=99;
+    kq[2]=99;
+    return kq;
   }else{
-    return (int)t;
+    kq[0]=(int)t;
+    kq[1]=(int)h;
+    kq[2]=(int)dht.computeHeatIndex(t,h,false);
+    return kq;
   }
 }
 
-int getHumid(){
-  float h=dht.readHumidity();
-  if(isnan(h)){
-    return 99;
-  }else{
-    return (int)h;
-  }
-}
 
 int nPos=0;
 void scrollTime(String thoigian, int spd){
@@ -99,107 +110,98 @@ void scrollDate(String ngaythang,int spd){
 //==============================================
 void handleRoot() {
  String s = MAIN_page; //Read HTML contents
- String stat="<br><p>Time Scroll: ";
- stat.concat(timescroll);
- stat+="<br>Date Scroll: ";
+ server.send(200, "text/html", s); //Send web page
+}
+//===============================================================
+
+void handleReset(){
+   datescroll=500;
+   autobrightness=true;
+  String s = "<a href='/'><font size='20'>Go Back</font></a>";
+  server.send(200, "text/html", s); //Send web page
+}
+
+void handleUpdateStatus(){
+  String stat="Date Scroll: ";
  stat.concat(datescroll);
- stat+="<br>Display Brightness: ";
+ stat+="<br>Intensity: ";
  stat.concat(brightness);
  stat+="<br>Auto Brightness: ";
  if(autobrightness){
-  stat+="yes";
+  stat+="ON";
  }else{
-  stat+="no";
+  stat+="OFF";
  }
  stat+="<br>Is Night: ";
  if(isNight){
-  stat+="yes";
+  stat+="ON";
  }else{
-  stat+="no";
+  stat+="OFF";
  }
- stat+="<br></p>";
- //Serial.print(stat);
- s.replace("@@@",stat);
- server.send(200, "text/html", s); //Send web page
-}
-//===============================================================
-// This routine is executed when you press submit
-//===============================================================
-void handleForm() {
- String TimeSpeed = server.arg("timescroll"); 
- String DateSpeed = server.arg("datescroll"); 
- if(TimeSpeed!=""){
- timescroll = TimeSpeed.toInt();
- if(timescroll>1000){
-  timescroll = 1000;
- }
- if(timescroll<100){
- timescroll = 100;
- }
- }else{
-  timescroll=500;
- }
- 
- if(DateSpeed!=""){
- datescroll = DateSpeed.toInt();
- if(datescroll>1000){
-  datescroll = 1000;
- }
- if(datescroll<100){
-  datescroll = 100;
- }
- }else{
-  datescroll = 500;
- }
- 
- String s = "<a href='/'><font size='20'>Go Back</font></a>";
- server.send(200, "text/html", s); //Send web page
+ stat+="<br>";
+  server.send(200,"text/plane",stat);
+  }
+
+void handleScroll() {
+  String DateSpeed = server.arg("scrollVal"); 
+ datescroll = abs(DateSpeed.toInt())*100;
 }
 
-void handleBrightness(){
- String Brightness =server.arg("brightness");
- if(autobrightness==false){
- if(Brightness!=""){
-  brightness=Brightness.toInt();
-  if(brightness<0){
-    brightness=0;
+void handleIntensity(){
+ String Brightness = server.arg("intensityVal");
+  if(autobrightness==false){
+  brightness=abs(Brightness.toInt());
   }
-  if(brightness>7){
-    brightness=7;
-  }
-  
- }else{
-  brightness=5;
- }
- }
-  String s = "<a href='/'><font size='20'>Go Back</font></a>";
-  server.send(200, "text/html", s); //Send web page
 }
 
-void handleAutobrightness(){
- String Autobrightness =server.arg("autobrightness");
- if(Autobrightness!=""){
-    if(Autobrightness=="yes"){
+void handleAutoIntensity(){
+ String AutoIntensity = server.arg("autoIntensity");
+  if(AutoIntensity=="ON"){
     autobrightness=true;   
-  }
-  if(Autobrightness=="no"){
-    autobrightness=false;  
-  }
- }else{
-  autobrightness=true; 
  }
-  String s = "<a href='/'><font size='20'>Go Back</font></a>";
-  server.send(200, "text/html", s); //Send web page
+ if(AutoIntensity=="OFF"){
+  autobrightness=false; 
+ }
 }
 
-void handleReset(){
-  timescroll=500;
-  datescroll=500;
-  //brightness=7;
-  autobrightness=true;
-  
-  String s = "<a href='/'><font size='20'>Go Back</font></a>";
-  server.send(200, "text/html", s); //Send web page
+void handleDisplayMode(){
+ String Display12hMode = server.arg("display12hMode");
+  if(Display12hMode=="ON"){
+    Mode24h=false;   
+ }
+ if(Display12hMode=="OFF"){
+  Mode24h=true; 
+ }
+}
+
+void handleUpdate12h(){
+String stat="";
+if(Mode24h){
+  stat="false";
+}else{
+  stat="true";
+}
+  server.send(200,"text/plane",stat);
+}
+
+void handleUpdateAI(){
+String stat="";
+if(autobrightness){
+  stat="true";
+}else{
+  stat="false";
+}
+  server.send(200,"text/plane",stat);
+}
+
+void handleUpdateScroll(){
+String stat=String(datescroll/100);
+  server.send(200,"text/plane",stat);
+}
+
+void handleUpdateIntensity(){
+String stat=String(brightness);
+  server.send(200,"text/plane",stat);
 }
 
 //=================================================
@@ -215,29 +217,53 @@ void setup(){
   if(!wm.autoConnect("TM1650Clock")){
   
   }
-  
+  display.println("----");
   timeClient.begin();
   timeClient.update();
-  
-  dt=getDate();
-  server.on("/", handleRoot);      //Which routine to handle at root location
-  server.on("/action_page", handleForm); //form action is handled here
-  server.on("/reset_page", handleReset);
-  server.on("/brightness_page", handleBrightness);
-  server.on("/autobrightness_page", handleAutobrightness);
+  _temphumid=getTempHumid();
+  sprintf(_temp,"%c %02d",'T',_temphumid[0]);//(char(26) mean degree C
+  sprintf(_humid,"%c %02d",'H',_temphumid[1]);
+  sprintf(_heatindex,"%c%c%02d",'h','i',_temphumid[2]);
+  _datetime=getDate();
+  _dow=dow[timeClient.getDay()];
+  server.on("/",handleRoot);      //Which routine to handle at root location
+  server.on("/reset_page",handleReset);
+  server.on("/setScroll",handleScroll); //form action is handled here
+  server.on("/setIntensity",handleIntensity);
+  server.on("/toggleAIMode",handleAutoIntensity);
+  server.on("/toggleDisplayMode",handleDisplayMode);
+  server.on("/updateStatus",handleUpdateStatus);
+  server.on("/update12hCheckbox",handleUpdate12h);
+  server.on("/updateAICheckbox",handleUpdateAI);
+  server.on("/updateScrollSlider",handleUpdateScroll);
+  server.on("/updateIntensitySlider",handleUpdateIntensity);
   server.begin();                  //Start server
   //Serial.println("Web server dang khoi dong. Vui long doi dia chi IP…");
   delay(1000);
   //Serial.println(WiFi.localIP());
+  char IPAddr[(uint8_t)20];
+    sprintf(IPAddr,"%d.%03d",WiFi.localIP()[2],WiFi.localIP()[3]);
+    if(timeClient.isTimeSet()){
+   display.println(IPAddr);
+    }
+    delay(2000);
 }
 
 void loop() {
   timeClient.update();
     server.handleClient();
     if(timeClient.getHours()==0){
-      dt=getDate();
+      _datetime=getDate();
+       _dow=dow[timeClient.getDay()];
     }
-   
+
+    int * t=getTime(Mode24h);
+  //strcpy(thoigian,getTimeString().c_str());
+    if(timeClient.getSeconds()%2){
+    sprintf(_thoigian,"%02d.%02d",t[2],t[1]);
+    }else{
+    sprintf(_thoigian,"%02d%02d",t[2],t[1]);  
+    }
    if((timeClient.getHours()>=0 && timeClient.getHours()<7)||(timeClient.getHours()>=22)){//auto set brightness to low in 22pm to 7am 
     isNight=true;
    }else{
@@ -255,17 +281,27 @@ void loop() {
     }else{
       display.setIntensity(brightness);
     }
+
+   if(timeClient.getMinutes()%2 && timeClient.getSeconds()%3){
+        _temphumid=getTempHumid();
+        sprintf(_temp,"%c %02d",'T',_temphumid[0]);//(char(26) mean degree C
+        sprintf(_humid,"%c %02d",'H',_temphumid[1]);
+        sprintf(_heatindex,"%c%c%02d",'h','i',_temphumid[2]);
+    }
     
-      if(timeClient.getSeconds()<25){
-        scrollTime(getTime(),timescroll);
-      }else if(timeClient.getSeconds()<40){
-          scrollDate(dt,datescroll);
-      }else if(timeClient.getSeconds()<50){
-          int nhietdo=getTemp();
-          display.setDisplayToDecNumber(nhietdo,0,false);
-      }else{
-          int doam=getHumid();
-          display.setDisplayToDecNumber(doam,0,false);  
+      if(timeClient.getSeconds()<20){
+        //scrollTime(getTime(),timescroll);
+        display.println(_thoigian);
+      }else if(timeClient.getSeconds()<30){
+          scrollDate(_datetime,datescroll);
+      }else if(timeClient.getSeconds()<35){      
+          display.println(_dow);
+      }else if(timeClient.getSeconds()<45){
+          display.println(_temp);  
+        }else if(timeClient.getSeconds()<55){
+          display.println(_humid);  
+        }else{
+          display.println(_heatindex);  
         }
       }
    
--- ESP8266_TM1650_NTP_Clock/myweb.h
+++ ESP8266_TM1650_NTP_Clock/myweb.h
@@ -3,6 +3,8 @@ const char MAIN_page[] PROGMEM = R"=====(
 <!DOCTYPE html>
 <html>
 <head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <style>
 p {
 color: navy;
@@ -11,37 +13,158 @@ text-transform: uppercase;
 </style>
 </head>
 <body>
-
-<h2>ESP8266 NTP Clock<h2>
+<h2>ESP8266 TM1650 NTP Clock<h2>
 <h3>Settings</h3>
 
-<form action="/action_page">
-  Time Scroll Speed
-  <input type="text" name="timescroll" value=""> (ms)
-  <br><br>
-  Date Scroll Speed
-  <input type="text" name="datescroll" value=""> (ms)
-  <br><br>
-  <input type="submit" value="Set">
-</form>
+<div>
+<span>Scroll Speed: </span><input type="range" min="1" max="10" class="slider" id="scrollSlider">
+</div>
 <br><br>
-<form action="/brightness_page">
-Display Brightness
-  <input type="text" name="brightness" value=""> (0-7)
-     <input type="submit" value="Set">
-</form> 
+<div>
+<span>Intensity: </span><input type="range" min="0" max="7" class="slider" id="intensitySlider">
+</div>
 <br><br>
-<form action="/autobrightness_page">
-Auto Brightness
-  <input type="text" name="autobrightness" value=""> (yes/no)
-     <input type="submit" value="Set">
-</form> 
+<div>
+<span>12h Mode: </span><input type="checkbox" id="mode12h" tabindex="0">
+</div>
+<br><br>
+<div>
+<span>Auto Intensity: </span><input type="checkbox" id="autointensity" tabindex="0">
+</div>
+<br><br>
+<div>
+Status:<br><span id="allstatus"></span>
+</div>
 <br><br>
 <font size='18' color='red'>Reset</font>
 <form action="/reset_page">
-     <input type="submit" value="Reset">
-</form> 
-@@@
+<input type="submit" value="Reset">
+</form>
+
+<script>
+setInterval(function(){receiveAllStatus();receiveDisplayMode();receiveAIMode();receiveScrollSlider();receiveIntensitySlider();},1000);
+
+document.getElementById("scrollSlider").oninput = function(){
+  var scrollVal=this.value;
+  var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+          if (this.readyState == 4 && this.status == 200) {
+          }
+        };
+        xhttp.open("GET","setScroll?scrollVal="+scrollVal,true);
+        xhttp.send();
+  };
+  
+document.getElementById("intensitySlider").oninput = function(){
+  var intensityVal=this.value;
+  var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+          if (this.readyState == 4 && this.status == 200) {
+          }
+        };
+        xhttp.open("GET","setIntensity?intensityVal="+intensityVal, true);
+        xhttp.send();
+  };
+  
+document.getElementById("mode12h").onclick = function() {
+        // access properties using this keyword
+        var display12hMode;
+        if (this.checked) {
+          display12hMode = "ON";
+        } else {
+          display12hMode = "OFF";
+        }
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+          if (this.readyState == 4 && this.status == 200) {
+            //console.log("Successfully received ");
+          }
+        };
+        
+        xhttp.open("GET","toggleDisplayMode?display12hMode="+display12hMode, true);
+        xhttp.send();
+      };
+      
+document.getElementById("autointensity").onclick = function() {
+        // access properties using this keyword
+        var autoIntensity;
+        if (this.checked) {
+          autoIntensity = "ON";
+        } else {
+          autoIntensity = "OFF";
+        }
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+          if (this.readyState == 4 && this.status == 200) {
+          }
+        };
+        xhttp.open("GET","toggleAIMode?autoIntensity="+autoIntensity, true);
+        xhttp.send();
+      };
+      
+function receiveAllStatus(){
+var xhttp=new XMLHttpRequest();
+xhttp.onreadystatechange=function(){
+  if(this.readyState==4 && this.status==200){
+document.getElementById("allstatus").innerHTML=this.responseText;
+  }
+};
+  xhttp.open("GET","updateStatus",true);
+  xhttp.send();
+}
+
+function receiveDisplayMode(){
+var xhttp=new XMLHttpRequest();
+xhttp.onreadystatechange=function(){
+if(this.readyState==4 && this.status==200){
+if(this.responseText=="true"){
+document.getElementById("mode12h").checked=true;
+}else{
+  document.getElementById("mode12h").checked=false;
+  }
+}
+};
+xhttp.open("GET","update12hCheckbox",true);
+xhttp.send();
+}
+
+function receiveAIMode(){
+var xhttp=new XMLHttpRequest();
+xhttp.onreadystatechange=function(){
+if(this.readyState==4 && this.status==200){
+if(this.responseText=="true"){
+document.getElementById("autointensity").checked=true;
+}else{
+  document.getElementById("autointensity").checked=false;
+  }
+}
+};
+xhttp.open("GET","updateAICheckbox",true);
+xhttp.send();
+}
+
+function receiveScrollSlider(){
+var xhttp=new XMLHttpRequest();
+xhttp.onreadystatechange=function(){
+if(this.readyState==4 && this.status==200){
+document.getElementById("scrollSlider").setAttribute("value",this.responseText);
+}
+};
+xhttp.open("GET","updateScrollSlider",true);
+xhttp.send();
+}
+
+function receiveIntensitySlider(){
+var xhttp=new XMLHttpRequest();
+xhttp.onreadystatechange=function(){
+if(this.readyState==4 && this.status==200){
+document.getElementById("intensitySlider").setAttribute("value",this.responseText);
+}
+};
+xhttp.open("GET","updateIntensitySlider",true);
+xhttp.send();
+}
+</script>
 </body>
 </html>
 )=====";
